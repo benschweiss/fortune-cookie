75ef69e0a4dba24524e5fdfb4ec7bcfa85d5e0a0
Ben Schweiss <benschweiss@gmail.com>
2017-02-01 21:34:35 -0600

    Initial Commit for Fortune Cookie assignment

--- main.py
+++ main.py
@@ -15,10 +15,36 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = [
+        "I see more programming in your future",
+        "Tomorrow's weather will be cold",
+        "Just quit",
+        "Do better",
+    ]
+
+    index = random.randint(0,3)
+    return fortunes[index]
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!!!!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = "<strong>" + str(random.randint(1,100)) + "</strong>"
+        lotto = "Your Lucky Number: " + lucky_number
+        lotto_paragraph = "<p>" + lotto + "</p>"
+
+        x_button = "<a href='.'><button>May I have another!?!?</button></a>"
+
+        content = header + fortune_paragraph + lotto_paragraph + x_button
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
